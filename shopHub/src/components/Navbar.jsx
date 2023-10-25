d5eb363d9938aa6e1698339b9ffdadc401b77d8f
monalinanda <monalinanda95@gmail.com>
2023-10-25 15:28:21 +0530

    change in navbar

--- shopHub/src/components/Navbar.jsx
+++ shopHub/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   const { totalQuantities , setShowCart ,showCart} = useStateContext();
   return (
-   <div className="flex justify-between bg-gradient-to-r from-cyan-500 to-blue-500 h-20 items-center">
+   <div className="flex justify-between bg-gradient-to-r from-cyan-500 to-blue-500 h-20 items-center p-5">
     <Link to="/"><h2 className="text-4xl font-extrabold  ">SHOPHUB STORE</h2></Link>
     <button type="button" className=" font-bold text-2xl" onClick={()=>setShowCart(true)}><AiOutlineShopping/>
     <span className="font-normal  text-base ">{totalQuantities}</span>
