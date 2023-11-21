c6368275baa6f41d5cb73694a292f7f0d566ff4b
monalinanda <monalinanda95@gmail.com>
2023-11-21 23:23:41 +0530

    url change

--- server/app.js
+++ server/app.js
@@ -29,8 +29,8 @@ app.post("/api/create-checkout-session", async (req, res) => {
     payment_method_types: ["card"],
     line_items: lineItems,
     mode: "payment",
-    success_url: "http://localhost:5173/success",
-    cancel_url: "http://localhost:5173/cancel",
+    success_url: "https://shop-hub-omega.vercel.app/success",
+    cancel_url: "https://shop-hub-omega.vercel.app/cancel",
   });
 
   res.json({ id: session.id });
